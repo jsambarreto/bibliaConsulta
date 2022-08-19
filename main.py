48430e973db335a40901759a77b43af5955c01f8
Jorge Barreto <jsambarret0@gmail..com>
2022-08-19 10:12:45 -0300

    fix: camelcase

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 from freebible import read_web
 import streamlit as st
+from re import sub
+
+book_=''
+term_=''
+
+def camel_case(s):
+  s = sub(r"(_|-)+", " ", s).title().replace(" ", "")
+  return ''.join([s[0].upper(), s[1:]])
 
 #with st.echo():
 st.text_input("Please, enter with Book", key="book")
@@ -12,10 +20,13 @@ def find_term_bible(book: str, term: str):
         for i in range(len(web[book][1])):
             find = str(web[book][1][i+1])
             if term in find:
-                st.write(web['John'][1][i+1])
+                st.write(web[book][1][i+1])
                 j+=1
         return f'Termo "{term}", ocorre {j} vezes no livro de {book}'
     else:
         return f'Informe os termos para busca'  
+if st.session_state.book!= '':
+    book_=camel_case(st.session_state.book)
 
-st.write(find_term_bible(book=st.session_state.book,term=st.session_state.term))
+
+st.write(find_term_bible(book=book_,term=st.session_state.term))
